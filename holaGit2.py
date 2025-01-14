882286eaf653724edf6d018aa5b3dc740ca9b148
Daniel <ludacar2799@gmail.com>
2025-01-13 20:50:10 -0500

    Este es mi segundo fichero version 2

--- holaGit2.py
+++ holaGit2.py
@@ -1,3 +1,3 @@
 #Codigo para manejo de control de versiones2
 
-print("Hola Mundo Git, este es mi ubicacion para el log 2")
+print("Hola Mundo Git, este es mi ubicacion para el log 2 con mi version 2")
